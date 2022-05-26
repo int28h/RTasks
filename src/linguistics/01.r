5b81d5d9a953fb0ba4b1d1c656aef7a31c97090a
Elena Listratova <int28h@protonmail.com>
2022-05-26 19:36:40 +0300

    (o^▽^o)

--- src/linguistics/01.r
+++ src/linguistics/01.r
@@ -8,7 +8,7 @@ exp(3)
 sin(3)
 cos(4)
 
-# choose(k, n) calculates the number of sets with n elements than can be chosen
+# choose(k, n) calculates the number of sets with n elements than can be chosen 
 # from a set with k elements, that is the binomial coefficient.
 choose(10, 3)
 
@@ -70,6 +70,53 @@ seq(0, 1, by = 0.01)
 # The Normal Distribution
 rnorm(100)
 
+# matrix
+matrix(1:16)
+matrix(1:16, ncol = 4)
+matrix(1:16, nrow = 2)
+
+matrix(1:16, nrow = 2, byrow = T)
+matrix(1:16, nrow = 2, byrow = T) * 3
+
+array(1:4, 1:4)
+
+c(1:7, "my") # strings
+c(1:7, F) # numbers
+
+list(c(T, F, T),
+     c("o", "a"),
+     c(1, 2))
+
+# Data.frame - все векторы должны быть одной длины
+
+df <- data.frame(logic = c(T, F, T),
+           letters = c("a", "b", "c"),
+           numbers = c(1, 2, 3),
+           stringsAsFactors = F)
+
+View(df)
+
+df$letters # returns factor without stringsAsFactors = F
+
+df[2,]
+df[,2]
+df[2:3, "numbers"]
+df[df$numbers > 2,]
+
+# functions
+my_function <- function(x, y){
+  return (x * 3 + y)
+}
+
+my_function(3, 3)
+
+is_odd <- function(number){
+  ifelse(number %% 2 == 1, T, F)
+}
+
+is_odd(4)
+is_odd(9)
+
 # Посчитайте сумму 1000 элементой арифметической прогрессии с шагом 9 и первым элементом 42.
 sum(seq(from = 42, length.out = 1000, by = 9))
 
@@ -78,6 +125,24 @@ sum(seq(from = 42, length.out = 1000, by = 9))
 choose(33, 3)
 
 # Проверьте векторизована ли функция library()
-# If library is called with no package or help argument, it lists all available packages
-# in the libraries specified by lib.loc, and returns the corresponding information
-# in an object of class "libraryIQR". 
+library(c(1, 2))
+# Error in library(c(1, 2)) : 'package' must be of length 1 - F
+
+# Установите пакет phonTools и введите команду pickIPA(n = 1). 
+# К какому ряду относится в выведенном на экран инвентаре гласный a?
+install.packages("phonTools")
+# library(phonTools)
+pickIPA(n = 1)
+
+# equation <- 3 - 2*pi*x + x**2
+polyroot(c(3, -2*pi, 1))
+
+integrand <- function(x){
+  return ((1 + 2 * x ** 2) * exp(x ** 2))
+}
+integrate(lower = -1, upper = 1, f = integrand)
+
+# n-й член последовательности фибоначчи
+fibonacci <- function(n){
+  tail((round(((5 + sqrt(5)) / 10) * (( 1 + sqrt(5)) / 2) ** (1:n - 1))), n = 1)
+}
